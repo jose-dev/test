e9220b0c4ebec78978201dc4ef48acc07226271f
jose.jimenez <jljc71@googlemail.com>
2014-12-30 16:50:04 +0000

    added other tables to support allocation and cubing.

--- ocd/tasks/induction/dbSchema/azp1b_allocation.sql
+++ ocd/tasks/induction/dbSchema/azp1b_allocation.sql
@@ -14,3 +14,31 @@ CREATE TABLE picks (
 );
 
 ALTER TABLE picks ADD CONSTRAINT pick_id_pk PRIMARY KEY (pick_id);
+
+
+--
+-- container table
+--
+DROP TABLE IF EXISTS containers;
+
+CREATE TABLE containers (
+    container_id  BIGINT,
+    order_id      BIGINT,
+    tag_id        VARCHAR
+);
+
+ALTER TABLE containers ADD CONSTRAINT container_id_pk PRIMARY KEY (container_id);
+
+
+--
+-- container table
+--
+DROP TABLE IF EXISTS container_picks;
+
+CREATE TABLE container_picks (
+    container_pick_id  BIGINT,
+    container_id       BIGINT,
+    pick_id            BIGINT
+);
+
+ALTER TABLE container_picks ADD CONSTRAINT container_pick_id_pk PRIMARY KEY (container_pick_id);
